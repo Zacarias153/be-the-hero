f937e764e955e6ac084079ad3dfd4bd189b3887f
zacarias <zacariaspereira@outlook.com.br>
2020-03-26 20:16:31 -0300

    segunda atualização

--- backend/src/controller/SessionController.js
+++ backend/src/controller/SessionController.js
@@ -2,15 +2,15 @@ const connection = require('../database/connection');
 
 module.exports = {
     async create(request,response){
-        const { id } = request.body;
+        const {id} = request.body;
 
         const ong = await connection('ongs')
-         .where('id',id)
-         .select('name')
-         .first();
-
+            .where('id',id)
+            .select('name')
+            .first();
+        
         if(!ong){
-            return response.status(400).json({erro:'No ONG found with  this ID'});
+            return response.status(400).json({essro:'No ONG found with this ID'});
         }
         return response.json(ong);
     }
